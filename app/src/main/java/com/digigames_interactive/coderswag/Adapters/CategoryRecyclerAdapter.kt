c8e26102c42d2790809be32b131345ca4ae0e215
Jovy Ong <jovy83@live.com>
2018-11-21 22:09:38 +0800

    refactored for recycler view adapter

---
+++ app/src/main/java/com/digigames_interactive/coderswag/Adapters/CategoryRecyclerAdapter.kt
@@ -0,0 +1,40 @@
+package com.digigames_interactive.coderswag.Adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.digigames_interactive.coderswag.Model.Category
+import com.digigames_interactive.coderswag.R
+import kotlinx.android.synthetic.main.category_list_item.view.*
+
+class CategoryRecyclerAdapter(val context: Context, val categories: List<Category>) : RecyclerView.Adapter<CategoryRecyclerAdapter.Holder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        // this is where we inflate the view for the very first time
+        // we can either pass the context from the constructor above, or use the parent.context
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.category_list_item, parent, false)
+
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindCategory(categories[position], context)
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView.categoryImage
+        val categoryName = itemView.categoryName
+
+        fun bindCategory(category: Category, context: Context) {
+            val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+            categoryImage.setImageResource(resourceId)
+            categoryName.text = category.title
+        }
+    }
+}
--- app/src/main/java/com/digigames_interactive/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/digigames_interactive/coderswag/Controller/MainActivity.kt
@@ -2,23 +2,27 @@ package com.digigames_interactive.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.digigames_interactive.coderswag.Adapters.CategoryAdapter
+import android.support.v7.widget.LinearLayoutManager
+import com.digigames_interactive.coderswag.Adapters.CategoryRecyclerAdapter
 import com.digigames_interactive.coderswag.R
 import com.digigames_interactive.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecyclerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
-
+        adapter = CategoryRecyclerAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
 
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
 
+        // if your cells won't dynamically change in size, set this property for improved performance
+        categoryListView.setHasFixedSize(true)
     }
 }
